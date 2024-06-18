7021fb7c8e286c26c944cc132cd8394f98d60ad3
Moe Khan <mobashir16@gmail.com>
2024-06-18 14:42:18 +0530

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,6 +9,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -19,7 +23,9 @@ const App = () => {
       <Display value={good} text='good' />
       <Display value={neutral} text='neutral' />
       <Display value={bad} text='bad' />
-
+      <Display value={all} text='all' />
+      <Display value={average} text='average' />
+      <Display value={`${positive} %`} text='positive' />
     </div>
   )
 }
