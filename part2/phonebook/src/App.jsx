c39ff2ccc0055a20ce05686ee6886c933b999a9b
Moe Khan <mobashir16@gmail.com>
2024-06-27 15:31:27 +0530

    2.12 Add data to server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -22,15 +22,20 @@ const App = () => {
     event.preventDefault()
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
-    } else {      
+    } else {
       const personObject = {
         name: newName,
         number: newNumber,
-        id: persons.length + 1
+        // id: `${persons.length + 1}`
       }
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+        })
     }
   }
 
