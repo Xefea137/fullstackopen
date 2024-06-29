732107985263e33e42b14f3983d0929baa43273a
Moe Khan <mobashir16@gmail.com>
2024-06-29 14:44:34 +0530

    2d

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,6 +1,8 @@
-const Persons = ({ name, number }) => {
+const Persons = ({ name, number, removePerson }) => {
   return (
-    <div>{name} {number}</div>
+    <div>
+      {name} {number} <button onClick={removePerson}>delete</button>
+    </div>
   )
 }
 
