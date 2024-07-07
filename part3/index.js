465df6baf2f8abf88abd0ef64ce48be1b8a1f7cf
Moe Khan <mobashir16@gmail.com>
2024-07-07 07:51:54 +0530

    3.9

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 let phonebook = [
   { 
@@ -25,6 +26,7 @@ let phonebook = [
   }
 ]
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 
@@ -96,6 +98,6 @@ app.post('/api/persons', morganDisplay, (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on post ${PORT}`);
