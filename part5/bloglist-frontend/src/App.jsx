7a6c456f6cb3407d4542a7c7fa5ae8b4310648bb
Moe Khan <mobashir16@gmail.com>
2024-08-01 11:32:44 +0530

    5.4

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -11,10 +11,12 @@ const App = () => {
   const [newTitle, setNewTitle] = useState('')
   const [newAuthor, setNewAuthor] = useState('')
   const [newUrl, setNewUrl] = useState('')
+  const [showNotification, setShowNotification] = useState({ message: null, type: null })
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
+    blogService
+      .getAll()
+      .then(blogs => setBlogs(blogs)
     )
   }, [])
 
@@ -30,48 +32,85 @@ const App = () => {
   const handleLogin = async (event) => {
     event.preventDefault()
 
-    const user = await loginService.login({
-      username, password
-    })
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+  
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+  
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')      
+    } catch (exception) {
+      setShowNotification({ message: exception.response.data.error, type: 'error' }) 
+      setTimeout(() => {
+        setShowNotification({ message: null, type: null })
+      }, 5000)
+    }
+  }
 
-    window.localStorage.setItem(
-      'loggedBlogappUser', JSON.stringify(user)
+  const loginForm = () => {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type='text'
+              value={username}
+              name='Username'
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type='password'
+              value={password}
+              name='password'
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type='submit'>login</button>
+        </form>
+      </div>
     )
-
-    blogService.setToken(user.token)
-    setUser(user)
-    setUsername('')
-    setPassword('')
   }
 
   const blogForm = () => (
     <form onSubmit={addBlog}>
+      <h2>Create new</h2>
       <div>
         Title:
-          <input
-            type='text'
-            value={newTitle}
-            name='title'
-            onChange={handleTitleChange}
-          />
+        <input
+          type='text'
+          value={newTitle}
+          name='title'
+          onChange={handleTitleChange}
+        />
       </div>
       <div>
         Author:
-          <input
-            type='text'
-            value={newAuthor}
-            name='author'
-            onChange={handleAuthorChange}
-          />
+        <input
+          type='text'
+          value={newAuthor}
+          name='author'
+          onChange={handleAuthorChange}
+        />
       </div>
       <div>
         Url:
-          <input
-            type='text'
-            value={newUrl}
-            name='url'
-            onChange={handleUrlChange}
-          />
+        <input
+          type='text'
+          value={newUrl}
+          name='url'
+          onChange={handleUrlChange}
+        />
       </div>
       <button type='submit'>create</button>
     </form>
@@ -87,12 +126,16 @@ const App = () => {
 
     blogService
       .create(blogObject)
-        .then(returnedBlog => {
-          setBlogs(blogs.concat(returnedBlog))
-          setNewTitle('')
-          setNewAuthor('')
-          setNewUrl('')
-        })
+      .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+        setNewTitle('')
+        setNewAuthor('')
+        setNewUrl('')
+        setShowNotification({ message: `A new blog ${newTitle} by ${newAuthor} added`, type: 'success' }) 
+        setTimeout(() => {
+          setShowNotification({ message: null, type: null })
+      }, 5000)
+      })
   }
 
   const handleTitleChange = (event) => {
@@ -112,43 +155,31 @@ const App = () => {
     setUser(null)
   }
 
-  if (user === null) {
+  const Notification = ({ message, type }) => {
+    if (message === null) {
+      return null
+    }
+  
     return (
-      <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={handleLogin}>
-          <div>
-            username
-              <input
-                type='text'
-                value={username}
-                name='Username'
-                onChange={({ target }) => setUsername(target.value)}
-              />
-          </div>
-          <div>
-            password
-            <input
-              type='password'
-              value={password}
-              name='password'
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type='submit'>login</button>
-        </form>
+      <div className={type}>
+        {message}
       </div>
     )
   }
 
   return (
     <div>
-      <h2>blogs</h2>
-      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
-      <h2>Create new</h2>
-      {blogForm()}
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      <Notification message={showNotification.message} type={showNotification.type} />
+      {!user && loginForm()}
+      {user && (
+      <div>
+        <h2>blogs</h2>
+        <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+        {blogForm()}
+        {blogs.map(blog => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
+      </div>
       )}
     </div>
   )
