f11ce9e0cafc539fefb186e366e065e662869a5a
Moe Khan <mobashir16@gmail.com>
2024-08-05 08:32:20 +0530

    5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [blogVisible, setBlogVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const showSomeDetails = { display: blogVisible ? 'none' : '' }
+  const showAllDetails = { display: blogVisible ? '' : 'none' }
+
+  const handleBlogView = () => {
+    setBlogVisible(!blogVisible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={showSomeDetails}>
+        {blog.title} {blog.author} <button onClick={handleBlogView}>View</button>
+      </div>
+      <div style={showAllDetails}>
+        <div>{blog.title} {blog.author} <button onClick={handleBlogView}>Hide</button></div>
+        <div>{blog.url}</div>
+        <div>{blog.likes} <button>Like</button></div>
+        <div>{blog.user.name}</div>
+      </div>      
+    </div>  
+  )
+}
 
 export default Blog
